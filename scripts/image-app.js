8461802e59b9177974e1d8617640efd17d78f68a
TRI VI <trivi2710@gmail.com>
2017-01-24 17:25:00 -0500

    fixing directory

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -35,7 +35,7 @@
   }
 
   //worker
-  var myWorker = new Worker("worker.js");
+  var myWorker = new Worker("script/worker.js");
 
   function manipulateImage(type) {
     var a, b, g, i, imageData, j, length, pixel, r, ref;
